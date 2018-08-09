c7e68c458752bfc0d42d1d14b07b3239f4226713
Oseias Beu <oseiasbeu@outlook.com>
2018-08-09 14:29:47 -0300

    Exercícios Java

---
+++ SpringBoot/spring/src/main/java/com/spring/spring/App.java
@@ -0,0 +1,18 @@
+package com.spring.spring;
+import java.util.List;
+
+import javax.persistence.Entity;
+
+/**
+ * Hello world!
+ *
+ */
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
