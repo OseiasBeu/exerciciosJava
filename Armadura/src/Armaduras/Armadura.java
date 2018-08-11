ee50ca886e14d8227a8935cc6694e864bb31d8e8
Oseias Beu <oseiasbeu@outlook.com>
2018-08-11 16:40:34 -0300

    Iniciar exercicio Armaduras

---
+++ Armadura/src/Armaduras/Armadura.java
@@ -0,0 +1,30 @@
+package Armaduras;
+
+
+
+public class Armadura {
+
+	private String cor;
+	private String nome;
+	private int quantidadeArmas;
+	public String getCor() {
+		return cor;
+	}
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getQuantidadeArmas() {
+		return quantidadeArmas;
+	}
+	public void setQuantidadeArmas(int quantidadeArmas) {
+		this.quantidadeArmas = quantidadeArmas;
+	}
+
+	
+}
---
+++ Armadura/src/Armaduras/Main.java
@@ -0,0 +1,18 @@
+package Armaduras;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Armadura armadura = new Armadura();
+		armadura.setNome("Mark");
+		armadura.setCor("red");
+		armadura.setQuantidadeArmas(20);
+		
+		System.out.println(armadura.getNome());
+		
+		
+	}
+
+
+
+}
